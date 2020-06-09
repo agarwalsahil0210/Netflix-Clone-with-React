57e2542f1711a915a77254b03bcee23bd9612c1d
Sahil Agarwal <agarwalsahil0210@gmail.com>
2020-06-09 20:16:01 +0530

    background and sign-in button added

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,53 @@ import styled from 'styled-components';
 class Header extends Component {
     render() {
         return (
-            <div className="header-container">
+            <HeaderComponent className="header-container">
                 <div className="header-top">
-                    <img src= {logo} />
+                    <Logo src= {logo} />
+                    <NavLink to className="signIn-btn">Sign In</NavLink>
                 </div>
-            </div>
+            </HeaderComponent>
         );
     }
 }
 
 export default Header;
+
+//Logo
+const Logo = styled.img`
+ width: 10rem;
+ height: 3.5rem;
+ position: absolute;
+ top: 25%;
+ left: 50%;
+ transform: translate(-50%, -50%);
+`;
+
+//Header Container
+const HeaderComponent = styled.div`
+ .signIn-btn{
+    right:0;
+    margin: 1.125rem 3% 0;
+    padding: 0.4375rem 1.0625rem;
+    font-weight: 400;
+    line-height: normal;
+    border-radius: 0.1875rem;
+    font-size: 1rem;
+    background: var(--main-red);
+    position: absolute;
+    translate: transform(-50%, -50%);
+    cursor: pointer;
+    transition: background 0.2s ease-in;
+    &:hover {
+        background: var(--main-red-hover);
+    }
+ }
+
+ //Header Top
+ .header-top {
+     position: relative;
+     height: 10rem;
+     z-index: 1;
+ }
+`;
 
